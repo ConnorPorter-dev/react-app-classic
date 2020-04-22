e695c5813ddfeffb3c8c0dcdccdbc2cc7ed1015a
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2020-04-22 15:05:47 +0100

    removed old data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import Counter from "./components/Counter/counter";
 import Header from "./components/Header/header";
 import CardSection from "./components/CardSection/cardSection";
 const data2 = require("./data.json");
-const data = [
-  {
-    title: "bigtitle",
-    desc: "descriptionarino",
-    imgsrc:
-      "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
-  },
-  {
-    title: "bigtitle",
-    desc: "descriptionarino",
-    imgsrc:
-      "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
-  },
-];
 
 class App extends Component {
   constructor(props) {
